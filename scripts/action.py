9762a7a23c5afaa921dc9142438d18b857ede17b
Shengjie Lin <StArchon@outlook.com>
2022-05-08 02:30:18 -0500

    update

--- scripts/action.py
+++ scripts/action.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
 import math
-import cv2
-
-# import the moveit_commander, which allows us to control the arms
-import moveit_commander
-import rospy
-from geometry_msgs.msg import Twist, Vector3
-from sensor_msgs.msg import LaserScan, Image
-import cv_bridge
-import numpy as np
-
-from utils import process_range_data
 import os
 
-path_prefix = os.path.dirname(__file__)  + "/action_states/"
-q_matrix_path = os.path.dirname(__file__)  + "/q_matrix.csv"
+import cv2
+import cv_bridge
+# import the moveit_commander, which allows us to control the arms
+import moveit_commander
+import numpy as np
+import rospy
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import Image, LaserScan
+
+from utils import process_range_data
+
+path_prefix = os.path.dirname(__file__) + "/action_states/"
+q_matrix_path = os.path.dirname(__file__) + "/q_matrix.csv"
 t_matrix_path = os.path.dirname(__file__) + "/transition_matrix.txt"
+
 
 class Robot(object):
     def __init__(self):
@@ -38,29 +39,27 @@ class Robot(object):
         self.pub_twist = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
         self.pub_img = rospy.Publisher("/debug", Image, queue_size=1)
         self.twist = Twist(linear=Vector3(), angular=Vector3())
-        self.ang_th = 30
 
         self.tgt_dist = 0.235
-        self.tgt_deg = -4
+        self.tgt_ang = -4
         self.k_dist = -0.3
-        # self.k_dist = -0.1
-        self.k_deg = -0.003
+        self.k_ang = -0.003
         # 0: identify colored object and move close to it
         # 1: grab object
         # 2: move to AR tag
         # 3: Drop Object
         # 4: Finished
         self.state = 0
-        self.deg = None
+        self.ang = None
         self.dist = None
         self.bridge = cv_bridge.CvBridge()
         self.error = None
         self.initialized = True
 
-        #import q matrix
+        # import q matrix
         self.q_matrix = np.loadtxt(q_matrix_path, delimiter=',')
         self.current_state = 0
-        
+
         # Fetch actions. These are the only 9 possible actions the system can take.
         # self.actions is an array of dictionaries where the row index corresponds
         # to the action number, and the value has the following form:
@@ -70,17 +69,23 @@ class Robot(object):
         self.actions = list(map(lambda x: {"object": colors[int(x[0])], "tag": int(x[1])}, self.actions))
         self.num_actions = len(self.actions)
 
-        self.transition_matrix = np.loadtxt(t_matrix_path)
+        self.transition_matrix = np.loadtxt(t_matrix_path).astype(int)
 
         self.next_object = None
         self.next_tag = None
 
         self.matched_object = 0
-    
+        self.lower_pink = np.array([154, 101.4, 75.8])
+        self.upper_pink = np.array([169, 152.6, 229.4])
+        self.lower_blue = np.array([91.5, 88.6, 101.4])
+        self.upper_blue = np.array([106.5, 139.8, 242.2])
+        self.lower_green = np.array([31.5, 88.6, 75.8])
+        self.upper_green = np.array([44, 152.6, 203.8])
+
     def get_action(self):
         max_score = np.max(self.q_matrix[self.current_state])
         optimal_actions = []
-        for i in range(len(self.num_actions)):
+        for i in range(self.num_actions):
             if self.q_matrix[self.current_state, i] == max_score:
                 optimal_actions.append(i)
         optimal_action = np.random.choice(optimal_actions)
@@ -88,158 +93,106 @@ class Robot(object):
         self.current_state = self.transition_matrix[self.current_state, optimal_action]
         self.next_object = dic["object"]
         self.next_tag = dic["tag"]
-        #return dic["object"], dic["tag"]
-
 
     def scan_callback(self, data):
         if not self.initialized:
             return
-        # if self.state not in {0, 1}:
-        #     return
-        retval = process_range_data(data)
-        # stop if nothing in range
-        # if retval is None:
-        #     self.pub_twist.publish(self.stop_twist)
-        #     return
-        self.deg, self.dist = retval
-        # print(retval)
-        # apply linear gain only if angular error is small enough
+        if self.state not in {0, 2}:
+            return
+        # retval = process_range_data(data, lo=-15, hi=16)
+        retval = process_range_data(data, lo=self.tgt_ang - 15, hi=self.tgt_ang + 16)
+        if retval is not None:
+            self.ang, self.dist = retval
 
     def img_callback(self, data):
         if not self.initialized:
             return
+        img = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
+        w = img.shape[1]
         if self.state == 0:
-            #finding object
-            img = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-            w = img.shape[1]
+            # finding colored object
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            lower_pink = np.array([154, 101.4, 75.8])
-            upper_pink = np.array([169, 152.6, 229.4])
-            lower_cyan = np.array([91.5, 88.6, 101.4])
-            upper_cyan = np.array([106.5, 139.8, 242.2])
-            lower_green = np.array([31.5, 88.6, 75.8])
-            upper_green = np.array([44, 152.6, 203.8])
             if self.next_object == "pink":
-                mask = cv2.inRange(hsv, lower_pink, upper_pink)
+                mask = cv2.inRange(hsv, self.lower_pink, self.upper_pink)
             elif self.next_object == "green":
-                mask = cv2.inRange(hsv, lower_green, upper_green)
+                mask = cv2.inRange(hsv, self.lower_green, self.upper_green)
             else:
-                mask = cv2.inRange(hsv, lower_cyan, upper_cyan)
-            # mask = cv2.inRange(hsv, lower_cyan, upper_cyan)
-            # mask = cv2.inRange(hsv, lower_green, upper_green)
+                mask = cv2.inRange(hsv, self.lower_blue, self.upper_blue)
             # self.pub_img.publish(self.bridge.cv2_to_imgmsg(mask, encoding='mono8'))
             M = cv2.moments(mask)
             if M['m00'] > 0:
-                # center of the yellow pixels in the image
+                # center of the colored pixels in the image
                 cx = int(M['m10'] / M['m00'])
                 cy = int(M['m01'] / M['m00'])
-                self.error = cx - w / 2
-                cv2.circle(img, (cx, cy), 20, (0, 0, 255), -1)
-                self.pub_img.publish(self.bridge.cv2_to_imgmsg(img, encoding='bgr8'))
-                # self.twist.angular.z = e * self.k_deg
-                # self.twist_pub.publish(self.twist)
-            else:
-                self.error = None
+                self.error = (cx - w / 2, rospy.Time.now())
+                cv2.circle(mask, (cx, cy), 10, 255, -1)
+                self.pub_img.publish(self.bridge.cv2_to_imgmsg(mask, encoding='mono8'))
         elif self.state == 2:
-            img = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-            w = img.shape[1]
-            #finding AR tag
+            # finding AR tag
             aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-            corners, ids, rejected_points = cv2.aruco.detectMarkers(gray, aruco_dict)
+            corners, ids, _ = cv2.aruco.detectMarkers(gray, aruco_dict)
             if ids is None:
                 return
             for id, corner_pts in zip(ids, corners):
-                corner_pts = corner_pts[0].astype(int)
-                print(id)
                 if id == self.next_tag:
+                    corner_pts = corner_pts[0].astype(int)
+                    cx = np.mean(corner_pts[:, 0])
+                    self.error = (cx - w / 2, rospy.Time.now())
                     # cv2.line(img, tuple(corner_pts[0]), tuple(corner_pts[1]), (255, 0, 0), 3)
                     # cv2.line(img, tuple(corner_pts[1]), tuple(corner_pts[2]), (255, 0, 0), 3)
                     # cv2.line(img, tuple(corner_pts[2]), tuple(corner_pts[3]), (255, 0, 0), 3)
                     # cv2.line(img, tuple(corner_pts[3]), tuple(corner_pts[0]), (255, 0, 0), 3)
-                    # self.pub_img.publish(self.bridge.cv2_to_imgmsg(img, encoding='bgr8'))
-                    cx = corner_pts[1][0] - corner_pts[0][0]
-                    # need to check if this is correct
-                    self.error = cx - w / 2
-                else:
-                    self.error = None
+                    cv2.polylines(img, [corner_pts], True, (255, 0, 0), 3)
+                    self.pub_img.publish(self.bridge.cv2_to_imgmsg(img, encoding='bgr8'))
+                    break
 
     def reset_to_middle(self):
-        self.twist.angular.z = math.radians(180)
+        self.twist.angular.z = math.radians(180 / 5)
         self.pub_twist.publish(self.twist)
-        rospy.sleep(1)
+        rospy.sleep(5)
         self.twist.angular.z = 0
         self.pub_twist.publish(self.twist)
         self.twist.linear.x = 0.1
         self.pub_twist.publish(self.twist)
-        rospy.sleep(3)
+        rospy.sleep(5)
         self.twist.linear.x = 0
-        self.pub_twist.publist(self.twist)
+        self.pub_twist.publish(self.twist)
 
     def run(self):
-        while not rospy.is_shutdown():
-            # print(self.state)
-            self.get_action()
-            # 0: identify colored object and move close to it
-            # 1: grab object
-            # 2: move to AR tag
-            # 3: Drop Object
-            # 4: Finished
-            if self.state == 0 or self.state == 2:
-                if self.dist is None or self.deg is None:
-                    continue
-                if self.error is None:
-                    # print('nothing detected, keep rotating')
-                    self.twist.angular.z = 0.3
-                    self.twist.linear.x = 0
-                else:
-                    self.twist.angular.z = self.error * self.k_deg
-                    if abs(self.tgt_deg - self.deg) < 30:
-                        if self.state == 0:
-                            if abs(self.tgt_dist - self.dist) < 0.003:
-                                self.twist.linear.x = 0
-                                self.twist.angular.z = 0
-                                self.pub_twist.publish(self.twist)
-                                self.state = 1
-                                continue
-                        if self.state == 2:
-                            if abs(self.tgt_dist + 0.05 - self.dist) < 0.003:
-                                self.twist.linear.x = 0
-                                self.twist.angular.z = 0
-                                self.pub_twist.publish(self.twist)
-                                self.state = 3
-                                continue
-                        # print('heading to object using k_dist')
-                        self.twist.linear.x = (self.tgt_dist - self.dist) * self.k_dist
-                        # print(self.twist.linear.x)
-                    else:
-                        # print('heading to object using const v')
-                        self.twist.linear.x = 0.1
-                        # self.twist.linear.x = 0.0
-                self.pub_twist.publish(self.twist)
-            # elif self.state == 1:
-            #     # if self.dist is None or self.deg is None:
-            #     #     continue
-            #     dist_diff = self.tgt_dist - self.dist
-            #     deg_diff = self.tgt_deg - self.deg
-            #     if abs(dist_diff) < 0.003 and abs(deg_diff) <= 1:
-            #         self.twist.linear.x = 0
-            #         self.twist.angular.z = 0
-            #         self.pub_twist.publish(self.twist)
-            #         self.state = 2
-            #         continue
-            #     self.twist.linear.x = dist_diff * self.k_dist if abs(self.deg) < self.ang_th else 0
-            #     # apply angular gain
-            #     self.twist.angular.z = deg_diff * self.k_deg
-            #     self.pub_twist.publish(self.twist)
-            elif self.state == 1:
-                #Picking up Object
-                gripper_joint_goal = [0.019, 0.019]
-                self.move_group_gripper.go(gripper_joint_goal, wait=True)
-                self.move_group_gripper.stop()
-                rospy.sleep(1)
+        gripper_joint_goal = [0.019, 0.019]
+        self.move_group_gripper.go(gripper_joint_goal)
+        self.move_group_gripper.stop()
 
+        tgt_dist = self.tgt_dist
+        self.get_action()
+        while not rospy.is_shutdown():
+            print(self.state)
+            # print(self.state)
+            # 0: spin, find colored object and move to it
+            # 1: grab object
+            # 2: spin, find AR tag and move to it
+            # 3: Drop Object
+            if self.state in {0, 2}:
+                if self.error is None or (rospy.Time.now() - self.error[1]).to_sec() > 0.3:
+                    # print('nothing detected, keep spinning')
+                    self.twist.linear.x = 0
+                    self.twist.angular.z = 0.3
+                else:
+                    if abs(self.error[0]) < 30 and self.dist is not None:
+                        if abs(tgt_dist - self.dist) < 0.01:
+                            self.twist.linear.x = 0
+                            self.twist.angular.z = 0
+                            self.pub_twist.publish(self.twist)
+                            self.state += 1
+                            continue
+                        self.twist.linear.x = (tgt_dist - self.dist) * self.k_dist
+                    else:
+                        self.twist.linear.x = 0
+                    self.twist.angular.z = self.error[0] * self.k_ang
+                self.pub_twist.publish(self.twist)
+            elif self.state == 1:
+                # Picking up Object
                 arm_joint_goal = [
                     0.0,
                     math.radians(30.0),
@@ -264,12 +217,12 @@ class Robot(object):
                 self.move_group_arm.go(arm_joint_goal, wait=True)
                 self.move_group_arm.stop()
                 rospy.sleep(5)
-                # we should a 180 here and move for 3 seconds
+                # reset
                 self.reset_to_middle()
-                self.state = 2
-
+                tgt_dist += 0.3
+                self.state += 1
             elif self.state == 3:
-                #Dropping off Object
+                # Dropping off Object
                 arm_joint_goal = [
                     0.0,
                     math.radians(30.0),
@@ -293,56 +246,18 @@ class Robot(object):
                 ]
                 self.move_group_arm.go(arm_joint_goal, wait=True)
                 self.move_group_arm.stop()
-                rospy.sleep(3)
-                #reset 180 again
+                # rospy.sleep(8)
+                # reset
+                self.reset_to_middle()
                 self.matched_object += 1
                 if self.matched_object == 3:
-                    self.state = 4
-                    continue
-                self.reset_to_middle()
+                    gripper_joint_goal = [0, 0]
+                    self.move_group_gripper.go(gripper_joint_goal, wait=True)
+                    self.move_group_gripper.stop()
+                    return
+                tgt_dist -= 0.3
                 self.state = 0
-        # # We can use the following function to move the arm
-        # # self.move_group_arm.go(arm_joint_goal, wait=True)
-
-        # # arm_joint_goal is a list of 4 radian values, 1 for each joint
-        # # wait=True ensures that the movement is synchronous
-
-        # # Let's move the arm based on what we have learned
-
-        # # First determine at what angle each joint should be.
-        # # You can use the GUI to find appropriate values based on your need.
-        # arm_joint_goal = [
-        #     0.0,
-        #     math.radians(5.0),
-        #     math.radians(10.0),
-        #     math.radians(-20.0)
-        # ]
-
-        # # Move the arm
-        # self.move_group_arm.go(arm_joint_goal, wait=True)
-
-        # # The above should finish once the arm has fully moved.
-        # # However, to prevent any residual movement,we call the following as well.
-        # self.move_group_arm.stop()
-
-        # # We can use the following function to move the gripper
-        # # self.move_group_gripper.go(gripper_joint_goal, wait=True)
-
-        # # gripper_joint_goal is a list of 2 values in meters, 1 for the left gripper and one for the right
-        # # wait=True ensures that the movement is synchronous
-
-        # # Let's move the gripper based on what we have learned
-
-        # # First determine what how far the grippers should be from the base position.
-        # # You can use the GUI to find appropriate values based on your need.
-        # gripper_joint_goal = [0.00, 0.00]
-
-        # # Move the gripper
-        # self.move_group_gripper.go(gripper_joint_goal, wait=True)
-
-        # # The above should finish once the arm has fully moved.
-        # # However, to prevent any residual movement,we call the following as well.
-        # self.move_group_gripper.stop()
+                self.get_action()
 
 
 if __name__ == '__main__':
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,45 +1,14 @@
-import numpy as np
-
-
-def process_range_data(data, k=7):
-    # # data: LaserScan
-    # # k: window size for mean-filtering data.ranges
-    # ranges = np.array(data.ranges)
-    # is_valid = np.logical_and(data.range_min <= ranges, ranges <= data.range_max)
-    # if not np.any(is_valid):
-    #     return None
-    # ranges[np.logical_not(is_valid)] = 0
-    # cs = np.cumsum(ranges)
-    # n_cv = np.cumsum(is_valid)
-    # # mean-filter the ranges to make it more robust
-    # avg_ranges = np.empty_like(ranges)
-    # for i in range(360):
-    #     l = i - k // 2
-    #     r = i + (k + 1) // 2
-    #     if l < 0:
-    #         n_valid = n_cv[-1] - n_cv[l + 359] + n_cv[r - 1]
-    #         sum_range = cs[-1] - cs[l + 359] + cs[r - 1]
-    #     elif r > 360:
-    #         n_valid = n_cv[-1] - n_cv[l - 1] + n_cv[r - 361]
-    #         sum_range = cs[-1] - cs[l - 1] + cs[r - 361]
-    #     else:
-    #         n_valid = n_cv[r - 1] - (n_cv[l - 1] if l else 0)
-    #         sum_range = cs[r - 1] - (cs[l - 1] if l else 0)
-    #     avg_ranges[i] = sum_range / n_valid if n_valid else np.nan
-    # # get the degree and distance of the closest point
-    # deg = np.nanargmin(avg_ranges)
-    # dist = avg_ranges[deg]
-    # if deg >= 180:
-    #     deg -= 360
-    # return deg, dist
-
+def process_range_data(data, lo=-180, hi=180):
+    # data: LaserScan
     min_dist = None
-    deg = 0
     for i in range(360):
-        if data.range_min < data.ranges[i] < data.range_max:
-            if min_dist is None or data.ranges[i] < min_dist:
-                deg = i
-                min_dist = data.ranges[i]
+        if i < hi or i - 360 >= lo:
+            if data.range_min < data.ranges[i] < data.range_max:
+                if min_dist is None or data.ranges[i] < min_dist:
+                    deg = i
+                    min_dist = data.ranges[i]
+    if min_dist is None:
+        return None
     if deg >= 180:
         deg -= 360
     return deg, min_dist
