dae1df36e5e98cbad04a683ee810bcb8058bb9ce
Shengjie Lin <StArchon@outlook.com>
2022-05-08 15:25:32 -0500

    more comments

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,14 +1,15 @@
 def process_range_data(data, lo=-180, hi=180):
+    # returns the angle and distance of the closest object within the angular range [lo, hi)
     # data: LaserScan
     min_dist = None
     for i in range(360):
         if i < hi or i - 360 >= lo:
             if data.range_min < data.ranges[i] < data.range_max:
                 if min_dist is None or data.ranges[i] < min_dist:
-                    deg = i
+                    ang = i
                     min_dist = data.ranges[i]
     if min_dist is None:
         return None
-    if deg >= 180:
-        deg -= 360
-    return deg, min_dist
+    if ang >= 180:
+        ang -= 360
+    return ang, min_dist
